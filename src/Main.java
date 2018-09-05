fcd793600a09546f68e8296a4bba83cd749123d4
Christian T Flink <chri62aa@edu.easj.dk>
2018-09-05 21:16:14 +0200

    første printf med flere variable

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String greeting = "Hej Verden!";
+        String name = "Christian";
+        String besked = "Og jeg synes programmering er for sindsygt fedt!";
+        int age = 32;
+        System.out.printf("%s, jeg hedder %s, jeg er %d år gammel.%n. %s", greeting, name, age, besked);
+
     }
+
 }
