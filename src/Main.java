720bc2007e9b1846173a14ad1839b0e57cd3862d
Christian T Flink <chri62aa@edu.easj.dk>
2018-09-17 09:37:06 +0200

    første printf med flere variable inkl. div. fra prof.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    static String name = "udenfor";
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -9,10 +13,25 @@ public class Main {
         String besked = "og jeg synes programmering er for sindsygt fedt!";
         int age = 32;
 
+        System.out.println(args[2]);
+
+        String[] test = new String[9];
+        test[1] = "0";
+
+        System.out.println(getName());
+
         System.out.printf("%s, jeg hedder %s, jeg er %d år gammel.%n%s", greeting, name, age, besked);
 
         Rectangle firkant = new Rectangle(5, 10, 20, 30);
         System.out.println(firkant);
+
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    void metodeName() {
 
     }
 }
